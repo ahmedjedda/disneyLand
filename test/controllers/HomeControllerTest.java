2d6e64039212d4c90ddb62c091488d950be5c4a6
ahmed jedda <ahmed.jedda@ensi-uma.tn>
2019-10-10 11:47:15 +0200

    Some Unit Tests

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,59 +1,87 @@
 package controllers;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.route;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
+import play.twirl.api.Content;
+import utils.AppUtils;
 
 public class HomeControllerTest extends WithApplication {
 
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder().build();
-    }
+	private static final String EXAMPLE_ONE_IMAGE = "https://picsum.photos/id/59/200/300";
+	private static final String EXAMPLE_ONE_IMAGE2 = "https://picsum.photos/id/60/200/300";
 
-    @Test
-    public void testIndex() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/");
+	private static final String EXAMPLE_HEADER = "/id/59/200/300";
+	private List<String> listOfUrls = Arrays.asList(EXAMPLE_ONE_IMAGE, EXAMPLE_ONE_IMAGE2);
 
-        Result result = route(app, request);
-        assertEquals(OK, result.status());
-    }
-    
-    @Test
-    public void testoneImage() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/test");
+	@Override
+	protected Application provideApplication() {
+		return new GuiceApplicationBuilder().build();
+	}
 
-        Result result = route(app, request);
-        assertNotNull(result);
-        assertEquals(OK, result.status());
-        
-        
-    }
-    
-    @Test
-    public void testMultipleImages() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/testMultiple");
+	@Test
+	public void testIndex() {
+		Http.RequestBuilder request = new Http.RequestBuilder().method(GET).uri("/");
 
-        Result result = route(app, request);
-        assertNotNull(result);
-        assertEquals(OK, result.status());
-        
-        
-    }
+		Result result = route(app, request);
+		assertEquals(OK, result.status());
+	}
+
+	@Test
+	public void testoneImage() {
+		Http.RequestBuilder request = new Http.RequestBuilder().method(GET).uri("/test");
+
+		Result result = route(app, request);
+		assertNotNull(result);
+		assertEquals(OK, result.status());
+
+	}
+
+	@Test
+	public void testMultipleImages() {
+		Http.RequestBuilder request = new Http.RequestBuilder().method(GET).uri("/testMultiple");
+
+		Result result = route(app, request);
+		assertNotNull(result);
+		assertEquals(OK, result.status());
+
+	}
+
+	@Test
+	public void TestgetIdFromMethod() {
+		String id = AppUtils.getIdFrom(EXAMPLE_HEADER);
+
+		assertEquals("59", id);
+
+	}
+
+	@Test
+	public void renderTemplateOneImage() {
+		Content html = views.html.image.render(EXAMPLE_ONE_IMAGE);
+		assertEquals("text/html", html.contentType());
+		assertTrue(contentAsString(html).contains("TASK 2"));
+	}
+
+	@Test
+	public void renderTemplateMultipleImages() {
+		Content html = views.html.images.render(this.listOfUrls);
+		assertEquals("text/html", html.contentType());
+		assertTrue(contentAsString(html).contains("TASK 3"));
+	}
 
 }
